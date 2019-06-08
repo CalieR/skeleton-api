c27d69d73677430f75b6b42aa483cf01116a5f19
CalieR <calielrushton@gmail.com>
2019-06-08 17:47:27 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+const port = process.env.PORT || 3002;
+
+app.use("/api", bodyParser.json());
+app.get("/api/health/ping", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", process.env.CORS_ALLOW_ORIGIN);
+  res.send({ result: "pong" });
+  //res.send()
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
